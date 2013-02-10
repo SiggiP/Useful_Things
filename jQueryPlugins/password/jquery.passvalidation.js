69fa8771b3db5fb59c104ddde5bba82662787fab
Siggi Paessler <siggi@tgwnn.de>
2013-02-10 18:12:55 +0100

    zwischenstand

--- jQueryPlugins/password/jquery.passvalidation.js
+++ jQueryPlugins/password/jquery.passvalidation.js
@@ -1,9 +1,14 @@
 !(function($){
     'use strict';
 	$.fn.passvalidation = function(){
-	   var options, colorize, checkweight, weight;
+	   var options, weight, colorize, checkweight, checkpassword;
 	   
-	   options = $.extend({},arguments[0]);
+	   options = $.extend({'minLength': 8
+	                      ,'regularExp': ''
+	                      ,'login': ''
+	                      ,'regularExpCallback': function(match){$.log('password '+(match?'matches':'doesn\'t match')+' regular expression');}
+	                      ,'weightCallback': function(weight){$.log('password weight is: '+weight);}
+	                      },arguments[0]);
 	   
 	   colorize = function(){
 	       $.log('colorize called: '+weight);
@@ -14,12 +19,44 @@
 	       colorize();
 	   };
 	   
+	   checkpassword = function(val){
+	       var digits, pwdLength, uCaseChars, lCaseChars, specialChars;
+	       pwdLength = val.length;
+	       if(pwdLength >= options.minLength){
+	           weight++;
+	       }
+	       digits = val.match(/(\d)/g);
+	       if(digits){
+	       	   weight += (pwdLength-digits.length>0?1:0);
+	       }
+	       uCaseChars = val.match(/[A-Z]/g);
+	       if(uCaseChars){
+	           weight += (pwdLength-uCaseChars.length>2?1:0);
+	       }
+	       lCaseChars = val.match(/[a-z]/g);
+           if(lCaseChars){
+               weight += (pwdLength-lCaseChars.length>2?1:0);
+           }
+	       specialChars = val.match(/(\W)/g);
+	       if(specialChars){
+	           weight += (pwdLength-specialChars.length>2?2:0);
+	       }
+	       if('' !== options.login){
+	           if(val.match(new RegExp(options.login,'g'))){
+	               weight -= (pwdLength-options.login.length === 0 ? weight : 1);	           	
+	           }
+	       }
+	       if(weight >= 6){
+	           $.log(val+' weight: '+weight);
+	       }
+	   };
+	   
 	   return this.each(function(){
 	       /* some magic */
-	   	   weight = 0;
 	   	   $(this).on('keyup', function(event){
-	   	       $.log('key'+event.keyCode+' => '+$(this).val());
-	   	       checkweight($(this).val());
+	   	       // $.log('key'+event.keyCode+' => '+$(this).val());
+	   	       weight = 0;
+               checkpassword($(this).val());
 	   	   });
 	   });
 	   
